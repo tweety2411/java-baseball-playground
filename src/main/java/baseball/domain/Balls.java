d64ca6b8ce3426d63b97444c74457c66f2ca54ef
HAE\1174321 <tweety2411@hyundai-autoever.com>
2022-10-12 13:30:59 +0900

    feat(baseball): 기능구현 완료

--- src/main/java/baseball/domain/Balls.java
+++ src/main/java/baseball/domain/Balls.java
@@ -6,19 +6,38 @@ public class Balls {
 
   // 비교해서 결과 리턴
   public String compare(List<Integer> computers, List<Integer> userBalls) {
-    return "";
+    if(isNothing(computers, userBalls)) return "낫싱";
+    return findMatchNumber(computers, userBalls);
   }
 
-  public boolean isStrike(List<Integer> computers, List<Integer> userBalls) {
-    return computers.equals(userBalls);
+  private String findMatchNumber(List<Integer> computers, List<Integer> userBalls) {
+    int strike = 0;
+    int ball = 0;
+    for(int i = 0; i < userBalls.size(); i++) {
+      if(isStrike(computers.get(i), userBalls.get(i))) {
+        strike++;
+        continue;
+      }
+      if(isBall(computers, userBalls.get(i))) ball++;
+    }
+    return printResult(ball, strike);
   }
 
-  public int ball(List<Integer> computers, List<Integer> userBalls) {
-  return 0;
+  public boolean isStrike(int computer, int user) {
+    return computer == user;
+  }
+  private boolean isBall(List<Integer> computers, int ball) {
+    return computers.contains(ball);
   }
 
-  // 아무숫자도 맞는 게 없으면 nothing
   public boolean isNothing(List<Integer> computers, List<Integer> userBalls){
     return userBalls.stream().noneMatch(computers::contains);
+  }
+
+  private String printResult(int ball, int strike) {
+    StringBuilder sb = new StringBuilder();
+    if(ball > 0) sb.append(ball).append("볼 ");
+    if(strike > 0) sb.append(strike).append("스트라이크");
+    return sb.toString().trim();
   }
 }
--- src/test/java/baseball/domain/BallsTest.java
+++ src/test/java/baseball/domain/BallsTest.java
@@ -4,20 +4,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class BallsTest {
+  private Balls balls;
+  private final List<Integer> computers = Arrays.asList(1, 2, 3);
+  @BeforeEach
+  void setUp(){
+    balls = new Balls();
+//    RandomNumber randomNumber = new RandomNumber();
+//    computers = randomNumber.generatorNumbers();
+  }
 
-  @Test
-  void isStrike() {
-    Balls balls = new Balls();
-    boolean result = balls.isStrike(Arrays.asList(1,2,3), Arrays.asList(1,2,3));
-    assertThat(result).isTrue();
+  @ParameterizedTest
+  @CsvSource({"3,2,1,2볼 1스트라이크", "1,2,3,3스트라이크", "3,1,2,3볼"})
+  void compare(int num1, int num2, int num3, String result) {
+    String actual = balls.compare(computers, Arrays.asList(num1, num2, num3));
+    assertThat(actual).isEqualTo(result);
   }
 
   @Test
   void isNothing() {
-    Balls balls = new Balls();
     boolean result = balls.isNothing(Arrays.asList(1,2,3), Arrays.asList(4, 5, 6));
     assertThat(result).isTrue();
   }
