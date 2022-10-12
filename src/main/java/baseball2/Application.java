5848fb9cc6703c0816b661ff2b089b24aabf9be2
HAE\1174321 <tweety2411@hyundai-autoever.com>
2022-10-12 10:24:03 +0900

    feat(baseball): 기능 완료

--- src/main/java/baseball2/Application.java
+++ src/main/java/baseball2/Application.java
@@ -1,14 +1,43 @@
 package baseball2;
 
 import baseball2.domain.Judgement;
+import baseball2.domain.NumberGenerator;
 import baseball2.domain.Referee;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
+/**
+ * 객체 지향 프로그래밍
+ * 1. 기능을 가지고 있는 클래스를 인스턴스화(객체)한다.
+ * 2. 필요한 기능을 (역할에 맞는) 인스턴스가 수행하게 한다.(의인화)
+ * 3. 각 결과를 종합한다.
+ *
+ */
 public class Application {
 
   public static void main(String[] args) {
+    NumberGenerator generator = new NumberGenerator();
+    List<Integer> computer = generator.createRandomNumbers();
+
     Referee referee = new Referee();
-    String result = referee.compare(Arrays.asList(7, 8, 9), Arrays.asList(1, 2, 3));
-    System.out.println(result);
+    String result = "";
+    while(!result.equals("0볼 3스트라이크")) {
+      result = referee.compare(computer, askNumber());
+      System.out.println(result);
+    }
+    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  }
+
+  public static List<Integer> askNumber() {
+    System.out.println("숫자를 입력해 주세요: ");
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.next();
+    List<Integer> numbers = new ArrayList<>();
+    for(String number : input.split("")){
+      numbers.add(Integer.valueOf(number));
+    }
+    return numbers;
   }
 }
